dc7c32b80cb1eb3575bd85b49a5e8228b09316c1
anton <aqw.novij@gmail.com>
2025-04-13 18:59:17 +0500

    release v.1

--- menu-extension@example.com/extension.js
+++ menu-extension@example.com/extension.js
@@ -1,7 +1,18 @@
+/**
+ * @typedef {import('@girs/st').St} St
+ * @typedef {import('@girs/clutter-13').Clutter} Clutter
+ * @typedef {import('@girs/gobject-2.0').GObject} GObject
+ * @typedef {import('@girs/gio-2.0').Gio} Gio
+ * @typedef {import('@girs/gtk-4.0').Gtk} Gtk
+ * @typedef {import('@girs/adw-1').Adw} Adw
+ */
+
+
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -19,45 +30,69 @@ const Indicator = GObject.registerClass(
             
             // Создаем иконку/текст для отображения в панели
             this.buttonText = new St.Label({
-                text: 'Опции',
+                text: 'Power Profiles',
                 y_align: Clutter.ActorAlign.CENTER
             });
             
             // Добавляем текст в панель
             this.add_child(this.buttonText);
             
-            // Создаем первый пункт меню
-            this._item1 = new PopupMenu.PopupSwitchMenuItem("Опция 1", 
-                this._settings.get_boolean('option1'));
+            // Добавляем подзаголовок в меню
+            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem('CPU/GPU Profiles'));
             
-            // Создаем второй пункт меню
-            this._item2 = new PopupMenu.PopupSwitchMenuItem("Опция 2", 
-                this._settings.get_boolean('option2'));
+            // Создаем пункты меню
+            this._item1 = new PopupMenu.PopupMenuItem("Conservative Mode");
+            this._item2 = new PopupMenu.PopupMenuItem("Powersave Mode");
+            
+            // Подключаем обработчики нажатий
+            this._item1.connect('activate', () => {
+                executeRootCommand('cpupower frequency-set -g conservative && echo "battery" > /sys/class/drm/card1/device/power_dpm_state && echo "auto" > /sys/class/drm/card1/device/power_dpm_force_performance_level');
+            });
+            
+            this._item2.connect('activate', () => {
+                executeRootCommand('cpupower frequency-set -g powersave && echo "battery" > /sys/class/drm/card1/device/power_dpm_state && echo "low" > /sys/class/drm/card1/device/power_dpm_force_performance_level');
+            });
             
             // Добавляем пункты в меню
             this.menu.addMenuItem(this._item1);
             this.menu.addMenuItem(this._item2);
-            
-            // Подключаем обработчики событий
-            this._item1.connect('toggled', (item, state) => {
-                this._settings.set_boolean('option1', state);
-            });
-            
-            this._item2.connect('toggled', (item, state) => {
-                this._settings.set_boolean('option2', state);
-            });
-
-            // Подключаем сигнал изменения настроек
-            this._settings.connect('changed::option1', () => {
-                this._item1.setToggleState(this._settings.get_boolean('option1'));
-            });
-
-            this._settings.connect('changed::option2', () => {
-                this._item2.setToggleState(this._settings.get_boolean('option2'));
-            });
         }
     }
 );
+
+function executeRootCommand(command) {
+    try {
+        // Асинхронный вызов не блокирует интерфейс
+        let proc = Gio.Subprocess.new(
+            ['pkexec', 'sh', '-c', command],
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+        
+        // Обрабатываем результат асинхронно
+        proc.communicate_utf8_async(null, null, (proc, res) => {
+            try {
+                let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                let status = proc.get_exit_status();
+                
+                if (status !== 0) {
+                    logError(new Error(`Error executing command: ${stderr}`));
+                    Main.notify(_("Ошибка выполнения команды"), stderr);
+                } else {
+                    log(`Command executed successfully`);
+                }
+            } catch (e) {
+                logError(e);
+                Main.notify(_("Ошибка выполнения команды"), e.message);
+            }
+        });
+        
+        return true;
+    } catch (e) {
+        logError(e);
+        Main.notify(_("Ошибка запуска команды"), e.message);
+        return false;
+    }
+}
 
 export default class MenuExtension extends Extension {
     enable() {
@@ -67,8 +102,11 @@ export default class MenuExtension extends Extension {
     }
     
     disable() {
-        this._indicator.destroy();
-        this._indicator = null;
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
         this._settings = null;
     }
 } 
+
